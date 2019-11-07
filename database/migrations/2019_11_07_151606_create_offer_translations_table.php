5cb8992d1831b641a5accbb5569774b46401c5e4
Davi Leichsenring <davi.leichsenring@21ilab.com>
2019-11-07 16:59:58 +0100

    init

---
+++ database/migrations/2019_11_07_151606_create_offer_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfferTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer_translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('offer_id')->unsigned();
+            $table->string('description');
+            $table->string('code', 2);
+            $table->timestamps();
+
+            $table->foreign('code')->references('code')->on('languages');
+            $table->foreign('offer_id')->references('id')->on('offers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer_translations');
+    }
+}
---
+++ database/migrations/2019_11_07_151631_create_subscription_translations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscription_translations', function (Blueprint $table) {
+
+          $table->bigIncrements('id');
+          $table->bigInteger('subscription_id')->unsigned();
+          $table->string('description');
+          $table->string('code', 2);
+          $table->timestamps();
+
+          $table->foreign('code')->references('code')->on('languages');
+          $table->foreign('subscription_id')->references('id')->on('subscriptions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription_translations');
+    }
+}
