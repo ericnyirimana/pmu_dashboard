3d26ff2b968299cea9b35e3de7c72b21367aab68
Davi Leichsenring <davi.leichsenring@21ilab.com>
2019-11-20 11:33:23 +0100

    adjust redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,9 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (!Auth::guard($guard)->check()) {
+        if (Auth::guard($guard)->check()) {
 
-            return redirect(route('dashboard.login'));
+            return redirect(route('dashboard.index'));
         }
 
         return $next($request);
