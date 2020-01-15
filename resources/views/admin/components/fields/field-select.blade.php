6127a8a5315f782292f6e2156bf9e73c74a82362
Davi Leichsenring <davi.leichsenring@21ilab.com>
2020-01-15 13:03:11 +0100

    adjust fields

--- resources/views/admin/components/fields/field-select.blade.php
+++ resources/views/admin/components/fields/field-select.blade.php
@@ -7,6 +7,16 @@
             <option value="{{ $value->id }}" @if (old( $foreignid, isset($model) && $model->$foreignid ) == $value->id) selected @endif>{{ $value->field_show }}</option>
           @endforeach
       </select>
-      @if(isset($help))<small id="{{ $field }}Help" class="form-text text-muted">{{ $help }}</small>@endif
     @endif
+    @if ($type=='simple')
+    
+      <select id="{{ $field }}" class="form-control" name="{{ $field }}" aria-describedby="{{ $field }}Help" @if(isset($required)) parsley-trigger="change" required @endif>
+          <option value="">Select {{ $label }}</option>
+          @foreach($values as $value)
+            <option value="{{ $value }}" @if (old( $field, $model->$field ) == $value) selected @endif>{{ $value }}</option>
+          @endforeach
+      </select>
+    @endif
+
+    @if(isset($help))<small id="{{ $field }}Help" class="form-text text-muted">{{ $help }}</small>@endif
 </div>
--- resources/views/admin/components/fields/field-text.blade.php
+++ resources/views/admin/components/fields/field-text.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
     <label for="{{ $field }}">{{ $label }}</label>
-    <input type="text" class="form-control" name="{{ $field }}" id="{{ $field }}" aria-describedby="{{ $field }}Help" placeholder="insert {{ $label }} here" value="{{ old($field, isset($model) ? $model->$field : '') }}" @if(isset($required)) parsley-trigger="change" required @endif>
+    <input type="text" class="form-control" name="{{ $field }}" id="{{ $field }}" aria-describedby="{{ $field }}Help" placeholder="insert {{ $label }} here" value="{{ old($field, isset($model) ? $model->$field : '') }}" @if(isset($required)) parsley-trigger="change" required @endif  @if(isset($disabled) && ($model)) disabled @endif>
     @if(isset($help))<small id="{{ $field }}Help" class="form-text text-muted">{{ $help }}</small>@endif
 </div>
