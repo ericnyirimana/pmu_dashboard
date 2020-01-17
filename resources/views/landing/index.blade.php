85d7457546426096bc7b63a1d7c51c5877f66ed4
Davi Leichsenring <davi.leichsenring@21ilab.com>
2020-01-17 18:10:44 +0100

    adjust landing

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -89,11 +89,11 @@
                         <input type="email" name="email" class="form-control" data-cons-subject="email" required />
                     </div>
                     <div class="form-group col-12">
-                          <label>Oggeto*</label>
+                          <label>Oggetto*</label>
                           <input type="text" name="subject" data-cons-subject="subject" class="form-control" required />
                     </div>
                     <div class="form-group col-12">
-                          <label>Messagio*</label>
+                          <label>Messaggio*</label>
                           <input type="text" class="form-control input-textarea" name="message" data-cons-subject="message" required />
                     </div>
                     <div class="form-group form-checkbox">
@@ -102,10 +102,13 @@
                     </div>
                     <div class="form-group form-checkbox">
                       <input type="checkbox" class="form-custom-check" id="check-privacy" data-cons-preference="restaurant-privacy-policy" required>
-                      <label class="form-check-label" for="check-privacy">* Acconsento all’uso dei miei dati personali in accordo con la <a href="https://www.iubenda.com/privacy-policy/65092557" class="iubenda-nostyle no-brand iubenda-embed" title="Privacy Policy">Privacy Policy</a> del servizio</label>
+                      <label class="form-check-label" for="check-privacy">Acconsento all’uso dei miei dati personali in accordo con la <a href="https://www.iubenda.com/privacy-policy/65092557" class="iubenda-nostyle no-brand iubenda-embed" title="Privacy Policy">Privacy Policy</a> del servizio*</label>
                     </div>
                     <div class="form-group col-12">
                       <button type="submit" class="btn btn-pmu desactivated btn-block" id="send-restaurant-contact">Invia</button>
+                    </div>
+                    <div class="col-12">
+                          <label class="form-check-label">*Campi obbligatori</label>
                     </div>
                 </form>
 
@@ -145,8 +148,8 @@
         </div>
         <form id="form-client">
             <div class="form-group">
-                <label>La tua email</label>
-                <input type="email" name="email" class="form-control" data-cons-subject="email" />
+                <label>La tua email*</label>
+                <input type="email" name="email" class="form-control" data-cons-subject="email" required email />
             </div>
             <div class="form-group form-checkbox">
               <input type="checkbox" class="form-custom-check" id="client-check-newsletter" data-cons-preference="client-newsletter">
@@ -154,10 +157,13 @@
             </div>
             <div class="form-group form-checkbox">
               <input type="checkbox" class="form-custom-check" id="client-check-privacy" data-cons-preference="client-privacy-policy" required>
-              <label class="form-check-label" for="client-check-privacy">* Acconsento all’uso dei miei dati personali in accordo con la <a href="https://www.iubenda.com/privacy-policy/65092557" class="iubenda-nostyle no-brand iubenda-embed" title="Privacy Policy">Privacy Policy</a> del servizio</label>
+              <label class="form-check-label" for="client-check-privacy">Acconsento all’uso dei miei dati personali in accordo con la <a href="https://www.iubenda.com/privacy-policy/65092557" class="iubenda-nostyle no-brand iubenda-embed" title="Privacy Policy">Privacy Policy</a> del servizio*</label>
             </div>
             <div class="form-group col-12">
               <button type="submit" class="btn btn-pmu desactivated btn-block" id="send-client-contact">Tienimi aggiornato</button>
+            </div>
+            <div class="col-12">
+                  <label class="form-check-label">*Campo obbligatorio</label>
             </div>
         </form>
       </div>
