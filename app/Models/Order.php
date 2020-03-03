fe78f4e3de228f4784ed10c81687633f636e97ea
Davi Leichsenring <davi.leichsenring@21ilab.com>
2020-03-03 15:00:57 +0100

    add Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+
+
+    public function user() {
+
+          return $this->hasOne('App\Models\User');
+
+    }
+
+
+    public function pickup() {
+
+          return $this->hasOne('App\Models\Pickup');
+
+    }
+
+
+    public function products() {
+
+          return $this->hasMany('App\Models\Product');
+
+    }
+
+}
