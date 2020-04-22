c353397b91dc05be5e8e80ccfece402d7cafd27e
Luca Raccampo <luca.raccampo@valuepartners.com>
2020-04-22 17:13:29 +0200

    fix user company relation, owner can use dashboard

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -5,132 +5,165 @@ namespace App\Http\Controllers;
 use App\Models\Company;
 use App\Models\User;
 use Auth;
+use function foo\func;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
 
 
-      public function __construct() {
+    public function __construct()
+    {
 
         $this->authorizeResource(Company::class);
 
-      }
+    }
 
 
-      public function validation(Request $request, $company = null) {
+    public function validation(Request $request, $company = null)
+    {
 
-          $request->validate(
+        $request->validate(
             [
-              'media_id'  => (empty($company)?'required':'').'',
-              'name'  => 'required',
-              'vat'   => 'required',
+                'media_id' => (empty($company) ? 'required' : '') . '',
+                'name' => 'required',
+                'vat' => 'required',
             ]
-          );
+        );
 
-      }
-
-
-
-      public function index() {
-
-          $companies = Company::get();
-
-          if (Auth::user()->is_owner) {
-
-            return redirect( route('companies.edit', Auth::user()->company));
-          }
-
-          return view('admin.companies.index')
-          ->with( compact('companies') );
-
-      }
+    }
 
 
-      public function create() {
+    public function index()
+    {
 
-            $company = new Company();
-            $users = User::all();
+        $companies = Company::get();
 
-            return view('admin.companies.create')->with([
-              'company'   => $company,
-              'users'   => $users,
+        if (Auth::user()->is_owner) {
+
+            return redirect(route('companies.edit', Auth::user()->company));
+        }
+
+        return view('admin.companies.index')
+            ->with(compact('companies'));
+
+    }
+
+
+    public function create()
+    {
+
+        $company = new Company();
+        $users = User::where('role', 'OWNER')->get()->filter(function ($user) {
+            return $user->brand->count() <= 0;
+        });
+
+        return view('admin.companies.create')->with([
+                'company' => $company,
+                'users' => $users,
             ]
-            );
+        );
 
-      }
+    }
 
 
-      public function store(Request $request) {
+    public function store(Request $request)
+    {
 
-            $this->validation($request);
+        $this->validation($request);
 
-            $fields = $request->all();
+        $fields = $request->all();
 
-            $fields['status'] = $request->status ? true : false;
+        $fields['status'] = $request->status ? true : false;
 
-            Company::create($fields);
+        $company = Company::create($fields);
 
-            return redirect()->route('companies.index')->with([
-                  'notification' => 'Company saved with success!',
-                  'type-notification' => 'success'
-                ]);
+        if ($fields['owner_id']) {
+            User::find($fields['owner_id'])->brand()->sync($company->id);
+        }
 
-      }
+        return redirect()->route('companies.index')->with([
+            'notification' => 'Company saved with success!',
+            'type-notification' => 'success'
+        ]);
 
-      public function show(Company $company) {
+    }
 
-            $users = User::all();
+    public function show(Company $company)
+    {
 
-            return view('admin.companies.view')->with([
-              'company'     => $company,
-              'users'     => $users,
+        $users = User::all();
+
+        return view('admin.companies.view')->with([
+                'company' => $company,
+                'users' => $users,
             ]
-            );
+        );
 
-      }
+    }
 
 
-      public function edit(Company $company) {
+    public function edit(Company $company)
+    {
 
-            $users = User::all();
+        $users = User::where('role', 'OWNER')->get()->filter(function ($user) use ($company) {
+            $brands = $user->brand;
+            return $brands->count() <= 0 || $brands->filter(function ($brand) use ($company) {
+                    return $brand->id == $company->id;
+                });
+        });
 
-            return view('admin.companies.edit')->with([
-              'company'   => $company,
-              'users'   => $users
+        return view('admin.companies.edit')->with([
+                'company' => $company,
+                'users' => $users
             ]
-            );
+        );
 
-      }
+    }
 
-      public function update(Request $request, Company $company) {
+    public function update(Request $request, Company $company)
+    {
 
-            $this->validation($request, $company);
+        $this->validation($request, $company);
 
-            $fields = $request->all();
+        $fields = $request->all();
 
-            $fields['status'] = $request->status ? true : false;
+        $fields['status'] = $request->status ? true : false;
 
-            $company->update($fields);
+        //clean owner before set
+        $ownerOld = User::find($company->owner_id);
+        $company->update($fields);
 
-            return redirect()->route('companies.index')->with([
-                  'notification' => 'Company saved with success!',
-                  'type-notification' => 'success'
-                ]);
+        if ($fields['owner_id']) {
+            if ($ownerOld && $fields['owner_id'] != $ownerOld->id) {
+                //clean old relations
+                $ownerOld->brand()->sync([]);
+                $ownerOld->restaurant()->sync([]);
+            }
 
-      }
+            //set new one
+            User::find($fields['owner_id'])->brand()->sync($company->id);
+        }
+
+        return redirect()->route('companies.index')->with([
+            'notification' => 'Company saved with success!',
+            'type-notification' => 'success'
+        ]);
+
+    }
 
 
-      public function destroy(Company $company) {
+    public function destroy(Company $company)
+    {
 
-            $company->delete();
+        $company->delete();
 
-            return redirect()->route('companies.index')->with([
-                  'notification' => 'Company removed with success!',
-                  'type-notification' => 'warning'
-                ]);
+        return redirect()->route('companies.index')->with([
+            'notification' => 'Company removed with success!',
+            'type-notification' => 'warning'
+        ]);
 
-      }
+    }
 
     public function data(Company $company = null)
     {
@@ -140,8 +173,6 @@ class CompanyController extends Controller
         return response()->json(Company::all(), 200);
 
     }
-
-
 
 
 }
