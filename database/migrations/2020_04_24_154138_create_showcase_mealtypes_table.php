037f7110616e1cd1ed95d6584994ec9b3cd92873
Mirko Pilato <mirko.pilato@valuepartners.com>
2020-04-24 17:46:29 +0200

    Create new table mealtypes

---
+++ database/migrations/2020_04_24_154138_create_showcase_mealtypes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShowcaseMealtypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('showcase_mealtypes', function(Blueprint $table){
+            $table->increments('id');
+            $table->bigInteger('showcase_id')->unsigned();
+            $table->bigInteger('mealtype_id')->unsigned();
+
+            $table->foreign('showcase_id')->references('id')->on('showcases')->onDelete('cascade');
+            $table->foreign('mealtype_id')->references('id')->on('mealtypes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('showcase_mealtypes');
+    }
+}
