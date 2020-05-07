525e8708ed69d81e2505e444f85861ff1508939a
Luca Raccampo <luca.raccampo@valuepartners.com>
2020-05-07 17:46:45 +0200

    comment fillable array

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
 
+    //protected $fillable = ['total_commission', 'total_amount', 'subtotal_amount', 'discounted_price'];
 
     public function user() {
 
