3e00bda1bdaa6d1628f00e899ac9e1b399bbe4c5
Luca Raccampo <luca.raccampo@valuepartners.com>
2020-05-11 16:06:25 +0200

    sync

--- app/Http/Controllers/PickupSubscriptionController.php
+++ app/Http/Controllers/PickupSubscriptionController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\PickupSubscription;
+use Illuminate\Http\Request;
 
 class PickupSubscriptionController extends Controller
 {
 
-    public function show(PickupSubscription $pickupSubscription)
+    public function show(Request $request)
     {
+
+        $pickupSubscription = PickupSubscription::find($request->id)->first();
 
         return view('admin.subscriptions.view')->with([
                 'pickupSubscription'  => $pickupSubscription
