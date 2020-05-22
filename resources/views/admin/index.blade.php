5ca1abe0e992599b5c74b5c8fa7ca078980d9917
Luca Raccampo <luca.raccampo@valuepartners.com>
2020-05-22 15:27:58 +0200

    fix dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,14 +9,14 @@
             <div class="col-12 col-md-8">
                 <div class="card-box table-responsive h-100">
                     <h4 class="m-t-0 header-title"><b>{{ ucfirst(trans('datatable.list_pickups')) }}</b></h4>
-
+                    @if($pickups->count() > 0)
                     <datatable route='pickups' :collection="$pickups" :fields="[
                 'datatable.headers.name'  => 'name',
                 'datatable.headers.price'  => 'price',
                 'datatable.headers.quantity'  => 'quantity_offer'
                 ]"
                     />
-
+                    @endif
                     <a href="{{ route('pickups.index' )}}" class="btn btn-success waves-effect w-md waves-light pull-right">{{ ucfirst(trans('button.view_all')) }}</a>
                 </div>
             </div>
@@ -26,13 +26,13 @@
                         <a href="{{ route('companies.create' )}}" class="btn btn-success waves-effect w-md waves-light pull-right">{{ ucfirst(trans('button.new_company')) }}</a>
                     </div>
                     <h4 class="m-t-0 header-title"><b>{{ ucfirst(trans('datatable.list_companies')) }}</b></h4>
-
+                    @if($companies->count() >0)
                     <datatable route='companies' :collection="$companies" :fields="[
                 'ID' => 'id',
                 'datatable.headers.name' => 'name',
                 'datatable.headers.number_restaurants' => 'restaurants_quantity',
                 ]" />
-
+                    @endif
                     <a href="{{ route('companies.index' )}}" class="btn btn-success waves-effect w-md waves-light pull-right">{{ ucfirst(trans('button.view_all')) }}</a>
                 </div>
             </div>
@@ -57,7 +57,7 @@
                 <div class=" card-box table-responsive h-100">
 
                     <h4 class="m-t-0 header-title"><b>{{ ucfirst(trans('datatable.list_orders')) }}</b></h4>
-
+                    @if($ordersPickup->count() > 0)
                     <datatable route='orders-pickup' :collection="$ordersPickup" :fields="[
                     'datatable.headers.date'      => 'order:date_format',
                     'datatable.headers.hour'      => 'order:hour_format',
@@ -66,6 +66,7 @@
                     ]"
                   actions='view'
                     />
+                        @endif
 
                 </div>
             </div>
@@ -74,14 +75,14 @@
                 <div class=" card-box table-responsive h-100">
 
                     <h4 class="m-t-0 header-title"><b>Sto arrivando!</b></h4>
-
+                    @if($ordersPickup->count() > 0)
                     <datatable route='orders-pickup' :collection="$ordersPickup" :fields="[
                     'datatable.headers.hour'      => 'order:hour_format',
                     'datatable.headers.offer'      => 'pickup:name',
                     'datatable.headers.price' => 'pickup:price',
                     ]"
                     />
-
+                    @endif
                 </div>
             </div>
 
@@ -91,14 +92,14 @@
             <div class="col-12 col-md-8">
                 <div class="card-box table-responsive">
                     <h4 class="m-t-0 header-title"><b>{{ ucfirst(trans('datatable.list_pickups')) }}</b></h4>
-
+                    @if($pickups->count() > 0)
                     <datatable route='pickups' :collection="$pickups" :fields="[
                 'datatable.headers.name'  => 'name',
                 'datatable.headers.price'  => 'price',
                 'datatable.headers.quantity'  => 'quantity_offer'
                 ]"
                     />
-
+                    @endif
                     <div class="col-12 mb-3">
                         <a href="{{ route('pickups.index' )}}" class="btn btn-success waves-effect w-md waves-light pull-right">{{ ucfirst(trans('button.view_all')) }}</a>
                     </div>
