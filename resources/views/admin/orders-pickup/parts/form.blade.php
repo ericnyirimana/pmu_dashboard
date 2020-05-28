95b6b36637ee319ec5b539e0062c371e821e9ba2
Luca Raccampo <luca.raccampo@valuepartners.com>
2020-05-28 17:32:51 +0200

    gestione ordine commentata

--- resources/views/admin/orders-pickup/parts/form.blade.php
+++ resources/views/admin/orders-pickup/parts/form.blade.php
@@ -14,17 +14,17 @@
         <p><label>{{ ucfirst(trans('labels.price')) }}:</label> {{ $ordersPickup->pickup->price }} €</p>
     </div>
 
-    <div class="form-group d-flex align-items-center justify-content-between col-12 mt-5">
-        @if(Auth::user()->is_super || Auth::user()->is_manager)
-            @if($ordersPickup->order->status == 'PENDING')
-            <button type="submit" field="status" name="status" value="REJECTED" class="btn btn-block w-lg btn-primary col-5">{{ ucfirst(trans('button.reject')) }}</button>
-            <button type="submit" field="status" name="status" value="PAID" class="btn w-lg btn-success col-5">{{ ucfirst(trans('button.confirm')) }}</button>
-            @elseif($ordersPickup->order->status == 'PAID')
-            <button type="submit" field="status" name="status" value="REJECTED" class="btn btn-block w-lg btn-primary col-5">{{ ucfirst(trans('button.reject')) }}</button>
-            <button type="submit" field="status" name="status" value="CLOSED" class="btn w-lg btn-success col-5">{{ ucfirst(trans('button.order_closed')) }}</button>
-            @else
-            <button type="submit" field="status" name="status" value="PAID" class="btn w-lg btn-success col-5">{{ ucfirst(trans('button.confirm')) }}</button>
-            @endif
-        @endif
-    </div>
+{{--    <div class="form-group d-flex align-items-center justify-content-between col-12 mt-5">--}}
+{{--        @if(Auth::user()->is_super || Auth::user()->is_manager)--}}
+{{--            @if($ordersPickup->order->status == 'PENDING')--}}
+{{--            <button type="submit" field="status" name="status" value="REJECTED" class="btn btn-block w-lg btn-primary col-5">{{ ucfirst(trans('button.reject')) }}</button>--}}
+{{--            <button type="submit" field="status" name="status" value="PAID" class="btn w-lg btn-success col-5">{{ ucfirst(trans('button.confirm')) }}</button>--}}
+{{--            @elseif($ordersPickup->order->status == 'PAID')--}}
+{{--            <button type="submit" field="status" name="status" value="REJECTED" class="btn btn-block w-lg btn-primary col-5">{{ ucfirst(trans('button.reject')) }}</button>--}}
+{{--            <button type="submit" field="status" name="status" value="CLOSED" class="btn w-lg btn-success col-5">{{ ucfirst(trans('button.order_closed')) }}</button>--}}
+{{--            @else--}}
+{{--            <button type="submit" field="status" name="status" value="PAID" class="btn w-lg btn-success col-5">{{ ucfirst(trans('button.confirm')) }}</button>--}}
+{{--            @endif--}}
+{{--        @endif--}}
+{{--    </div>--}}
 </div>
